e3b18b1e4778c180652a66c153198e384e85f254
27293 <myltd6@fpt.com.vn>
2020-11-17 22:02:34 +0700

    create project

--- ShareSheet/ViewController.swift
+++ ShareSheet/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .link
+        button.setTitle("Tap Me", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        button.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+        button.center = view.center
+        button.addTarget(self, action: #selector(handlePresentShareSheet(_:)), for: .touchUpInside)
+        view.addSubview(button)
     }
 
+    @objc private func handlePresentShareSheet(_ sender: UIButton){
+        guard let image = UIImage(systemName: "bell"),let url = URL(string: "https://www.google.com") else {
+            return
+        }
+        let shareSheetVC = UIActivityViewController(activityItems: [image,url], applicationActivities: nil)
+        // Ipad support
+        shareSheetVC.popoverPresentationController?.sourceView = sender
+        shareSheetVC.popoverPresentationController?.sourceRect = sender.frame
+        present(shareSheetVC, animated: true)
+    }
 
 }
 
